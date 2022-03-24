7828fd8aaa43d345453435bec0c92ae7b80f7664
Shai Raz <shayy9999@gmail.com>
2022-03-24 18:51:03 +0200

    commit

---
+++ alogs.py
@@ -0,0 +1,282 @@
+import sys
+from heapq import heappush, heappop
+from time import sleep
+
+MAP_DICT = {
+    "STARTING_POINT": 'A',
+    "ENDING_POINT": 'B',
+    "WATER": 'W',
+    "PAVED_ROAD": 'R',
+    "NON_PAVED_ROAD": 'D',
+}
+
+COST_DICT = {
+    "A": 0,
+    "B": 0,
+    "R": 1,
+    "D": 4,
+}
+
+PATH_DICT = {
+    "UP": 'U',
+    "DOWN": 'D',
+    "LEFT": 'L',
+    "RIGHT": 'R'
+}
+
+
+def find_start_and_end(grid):
+    for j, row in enumerate(grid):
+        for i, cell in enumerate(row):
+            if cell == MAP_DICT["STARTING_POINT"]:
+                start = (i, j)
+            elif cell == MAP_DICT["ENDING_POINT"]:
+                end = (i, j)
+
+    return start, end
+
+
+def get_manhattan_distance(node, goal):
+    return abs(node[0] - goal[0]) + abs(node[1] - goal[1])
+
+
+def get_successors(node, closed, grid, size):
+    successors = []
+    x, y = node[0], node[1]
+
+    if (x + 1 < size) and (grid[y][x + 1] != MAP_DICT["WATER"]) and ((x+1, y) not in closed):
+        successors.append((x + 1, y))
+
+    if (x - 1 >= 0) and (grid[y][x - 1] != MAP_DICT["WATER"]) and ((x-1, y) not in closed):
+        successors.append((x - 1, y))
+
+    if (y + 1 < size) and (grid[y + 1][x] != MAP_DICT["WATER"]) and ((x, y+1) not in closed):
+        successors.append((x, y + 1))
+
+    if (y - 1 >= 0) and (grid[y - 1][x] != MAP_DICT["WATER"]) and ((x, y-1) not in closed):
+        successors.append((x, y - 1))
+
+    return successors
+
+# checks if the child is an ancestor of the node
+
+
+def is_ancestor(node, child, parents):
+    while node is not None:
+        if node == child:
+            return True
+        node = parents[node]
+
+
+class GreedyBestFirstSearch:
+    closed = []
+
+    def __init__(self, grid, size):
+        self.grid = grid
+        self.size = size
+
+        self.start, self.end = find_start_and_end(grid)
+
+    def get_successors(self, node, closed):
+        return get_successors(node, closed, self.grid, self.size)
+
+    def get_heuristic(self, node):
+        return get_manhattan_distance(node, self.end)
+
+    def get_path(self, node, parents):
+        path = []
+        cost = 0
+        while node is not None:
+            path.append(node)
+            node = parents[node]
+
+        path = path[::-1]
+        dir_path = []
+        for i in range(len(path)-1):
+            cost += COST_DICT[self.grid[path[i][1]][path[i][0]]]
+
+            new_x = path[i+1][0] - path[i][0]
+            new_y = path[i+1][1] - path[i][1]
+            if new_x == 1:
+                dir_path.append(PATH_DICT["RIGHT"])
+            elif new_x == -1:
+                dir_path.append(PATH_DICT["LEFT"])
+            elif new_y == 1:
+                dir_path.append(PATH_DICT["DOWN"])
+            elif new_y == -1:
+                dir_path.append(PATH_DICT["UP"])
+
+        return '-'.join(dir_path), cost
+
+    def run(self):
+        opened = []
+        closed = []
+
+        # dictionary to store parents (to get the path), key = node, value = parent
+        parents = {}
+        parents[self.start] = None
+
+        heappush(opened, (self.get_heuristic(self.start), self.start))
+
+        while len(opened) > 0:
+            # set current node
+            node = heappop(opened)[1]
+            closed.append(node)
+
+            # if the current node is the end node, return the path
+            if node == self.end:
+                return self.get_path(node, parents)
+
+            # get successors
+            successors = self.get_successors(node, closed)
+            for successor in successors:
+                h = self.get_heuristic(successor)
+                if (h, successor) not in opened:
+                    heappush(opened, (h, successor))
+                    parents[successor] = node
+
+        return "no path", 0
+
+
+class AStar:
+    def __init__(self, grid, size):
+        self.grid = grid
+        self.size = size
+        self.start, self.end = find_start_and_end(grid)
+
+    def get_successors(self, node):
+        return get_successors(node, [], self.grid, self.size)
+
+    def get_heuristic(self, node):
+        return get_manhattan_distance(node, self.end)
+
+    def get_g(self, node, parents):
+        g = 0
+        while node is not None:
+            g += COST_DICT[self.grid[node[1]][node[0]]]
+            node = parents[node]
+        return g
+
+    def get_path(self, node, parents):
+        path = []
+        cost = 0
+        while node is not None:
+            path.append(node)
+            node = parents[node]
+
+        path = path[::-1]
+        dir_path = []
+        for i in range(len(path)-1):
+            cost += COST_DICT[self.grid[path[i][1]][path[i][0]]]
+
+            new_x = path[i+1][0] - path[i][0]
+            new_y = path[i+1][1] - path[i][1]
+            if new_x == 1:
+                dir_path.append(PATH_DICT["RIGHT"])
+            elif new_x == -1:
+                dir_path.append(PATH_DICT["LEFT"])
+            elif new_y == 1:
+                dir_path.append(PATH_DICT["DOWN"])
+            elif new_y == -1:
+                dir_path.append(PATH_DICT["UP"])
+
+        return '-'.join(dir_path), cost
+
+    def run(self):
+        opened = []
+        closed = []
+
+        # dictionary to store parents (to get the path), key = node, value = parent
+        parents = {}
+        parents[self.start] = None
+
+        heappush(opened, (self.get_heuristic(self.start), self.start))
+
+        while len(opened) > 0:
+            # set current node
+            node = heappop(opened)[1]
+            closed.append(node)
+
+            # if the current node is the end node, return the path
+            if node == self.end:
+                return self.get_path(node, parents)
+
+            # get successors
+            successors = self.get_successors(node)
+            for successor in successors:
+                # make sure the child isnt an ancestor of the node,
+                # to avoid infinite looping
+                if is_ancestor(node, successor, parents):
+                    continue
+                new_parents = parents.copy()
+                new_parents[successor] = node
+                g = self.get_g(successor, new_parents)
+                h = self.get_heuristic(successor)
+                f = g + h
+                if (f, successor) not in opened and successor not in closed:
+                    heappush(opened, (f, successor))
+                    parents[successor] = node
+                else:
+                    # find the node in the opened list
+                    for i in range(len(opened)):
+                        if opened[i][1] == successor:
+                            # if the new f value is smaller, update the node
+                            if f < opened[i][0]:
+                                opened[i] = (f, successor)
+                                parents[successor] = node
+                                break
+
+        return "no path", 0
+
+
+def read_file(file_name):
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        algorithm, size, input_mat = lines[0].strip(
+        ), lines[1].strip(), lines[2:]
+
+    size = int(size)
+    grid = [[letter for letter in row.strip()] for row in input_mat]
+
+    return algorithm, size, grid
+
+
+def main():
+    if (len(sys.argv) != 2):
+        print('Usage: py assignment1.py <input_file>')
+        exit(1)
+
+    file_name = sys.argv[1]
+
+    algorithm, size, grid = read_file(file_name)
+
+    # presenting the input
+    print(f"Running {algorithm} on {size}x{size} matrix: ")
+    for row in grid:
+        for letter in row:
+            print(f"{letter} ", end='')
+        print()
+
+    path = ""
+    cost = 0
+
+    # running the alogrithm
+    if algorithm == 'A*':
+        astar = AStar(grid, size)
+        path, cost = astar.run()
+    elif algorithm == 'greedyBestFirst':
+        gbs = GreedyBestFirstSearch(grid, size)
+        path, cost = gbs.run()
+
+    print("path: ", path)
+    print("cost: ", cost)
+
+    with(open("output.txt", "w+")) as f:
+        if path == "no path":
+            f.write("no path")
+        else:
+            f.write(path + " " + str(cost))
+
+
+if __name__ == '__main__':
+    main()
---
+++ gui.py
@@ -0,0 +1,197 @@
+import tkinter as tk
+import alogs
+from alogs import AStar, GreedyBestFirstSearch, MAP_DICT
+import os
+import time
+
+COLOR_DICT = {
+    'R': 'Yellow',  # paved road
+    'D': 'Orange',  # non paved road
+    'W': 'Blue',  # water
+    'A': 'Green',  # start
+    'B': 'Red',  # end
+}
+
+COLOR_ID = ['R', 'W', 'D', 'A', 'B']  # order of cycling clicks
+
+
+class Frame():
+    grid = []
+    num_of_rows = 0
+    num_of_cols = 0
+    last_dragged = None
+
+    def __init__(self, root, *args, **kwargs):
+        self.root = root
+        self.menu = tk.Menu(root)
+        self.menu.add_command(label="Load", command=self.load)
+        self.menu.add_command(label="Save", command=self.save)
+        self.menu.add_command(label="Reset", command=self.reset)
+
+        algo_menu = tk.Menu(self.menu, tearoff=0)
+        algo_menu.add_command(
+            label="A*", command=lambda: self.setAlgorithm("A*"))
+        algo_menu.add_command(label="Greedy Best First Search",
+                              command=lambda: self.setAlgorithm("greedyBestFirst"))
+        self.menu.add_cascade(label="Algorithm", menu=algo_menu)
+        self.menu.add_command(label="Run", command=self.run)
+        root.config(menu=self.menu)
+
+        # Setup Canvas
+        self.c = tk.Canvas(root, height=600, width=600, bg='white')
+        self.c.pack(fill=tk.BOTH, expand=True)
+        self.c.bind('<Configure>', self.initGrid)
+        self.pixel_width = 20
+        self.pixel_height = 20
+
+        self.algorithm = "A*"  # default is astar
+
+        self.statusbar = tk.Label(
+            root, text=self.algorithm, bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        self.statusbar.pack(side=tk.BOTTOM, fill=tk.X)
+        # change statusbar text
+        # self.statusbar.config(text="Ready")
+
+        # Setup binds
+        self.c.bind("<ButtonPress-1>", self.leftClick)
+        self.c.bind("<B1-Motion>", self.leftClickDrag)
+        # self.c.bind("<ButtonRelease-1>", lambda x: print("Release"))
+
+    def updateStatusBar(self):
+        self.statusbar.config(text=self.algorithm)
+
+    def setAlgorithm(self, algo):
+        self.algorithm = algo
+        self.updateStatusBar()
+
+    def getPosInGrid(self, item):
+        item = item[0] - 1
+        x = int(item % self.num_of_cols)
+        y = int(item / self.num_of_cols)
+        return x, y
+
+    def updateCell(self, items, drag=False):
+        x, y = self.getPosInGrid(items)
+        # print(f"item: {items[0]-1}, x, y: {x}, {y}")
+
+        if drag:
+            if self.last_dragged == (x, y):
+                return
+        self.last_dragged = (x, y)
+
+        next_tile = COLOR_ID[(COLOR_ID.index(
+            self.grid[y][x]) + 1) % len(COLOR_ID)]
+        next_color = COLOR_DICT[next_tile]
+        self.grid[y][x] = next_tile
+        # print(self.grid)
+
+        rect_id = items[0]
+        self.c.itemconfigure(rect_id, fill=next_color)
+
+    def leftClick(self, event):
+        items = self.c.find_closest(event.x, event.y)
+        if items:
+            self.updateCell(items)
+
+    def leftClickDrag(self, event):
+        items = self.c.find_closest(event.x, event.y)
+        if items:
+            self.updateCell(items, True)
+
+    def displayPath(self, path):
+        path = path.split("-")
+
+        start = alogs.find_start_and_end(self.grid)[0]
+        current = start
+
+        for move in path:
+            if move == "R":
+                current = (current[0] + 1, current[1])
+            elif move == "L":
+                current = (current[0] - 1, current[1])
+            elif move == "U":
+                current = (current[0], current[1] - 1)
+            elif move == "D":
+                current = (current[0], current[1] + 1)
+
+            if self.grid[current[1]][current[0]] == MAP_DICT["ENDING_POINT"]:
+                break
+            self.c.itemconfigure(
+                current[0] + current[1] * self.num_of_cols + 1, fill="magenta")
+
+    def updateGrid(self):
+        num_of_items = self.num_of_rows * self.num_of_cols
+        for i in range(num_of_items):
+            x = int(i % self.num_of_cols)
+            y = int(i / self.num_of_cols)
+            self.c.itemconfigure(i+1, fill=COLOR_DICT[self.grid[y][x]])
+
+    def createGrid(self):
+        for y in range(self.num_of_rows):
+            for x in range(self.num_of_cols):
+                x1 = (x * self.pixel_width)
+                x2 = (x1 + self.pixel_width)
+                y1 = (y * self.pixel_height)
+                y2 = (y1 + self.pixel_height)
+                self.c.create_rectangle(
+                    x1, y1, x2, y2, fill=COLOR_DICT[self.grid[y][x]])
+        self.c.update()
+
+    def initGrid(self, event=None):
+        self.num_of_rows = int(self.c.winfo_height() / self.pixel_height)
+        self.num_of_cols = int(self.c.winfo_width() / self.pixel_width)
+        print("Rows: " + str(self.num_of_rows))
+        print("Cols: " + str(self.num_of_cols))
+        self.grid = [[MAP_DICT["PAVED_ROAD"] for _ in range(
+            self.num_of_cols)] for _ in range(self.num_of_rows)]
+
+        self.createGrid()
+
+    def load(self):
+        self.algorithm, size, self.grid = alogs.read_file("input.txt")
+        self.num_of_rows = size
+        self.num_of_cols = size
+        self.updateStatusBar()
+        self.updateGrid()
+
+    def save(self):
+        with(open("input.txt", "w+")) as f:
+            f.write("A*\n")
+            f.write(str(self.num_of_rows) + "\n")
+
+            for row in self.grid:
+                f.write("".join(row) + "\n")
+            # remove last charachter
+            f.truncate(f.tell()-len(os.linesep))
+
+    def run(self):
+        self.updateGrid()
+        if self.algorithm == "A*":
+            astar = AStar(self.grid, self.num_of_rows)
+            start = time.time()
+            path, cost = astar.run()
+            end = time.time()
+            print(f"Time to run: {end-start}")
+        elif self.algorithm == "greedyBestFirst":
+            greedy = GreedyBestFirstSearch(self.grid, self.num_of_rows)
+            path, cost = greedy.run()
+        print(path)
+        print(cost)
+        self.displayPath(path)
+
+    def reset(self):
+        for y in range(self.num_of_rows):
+            for x in range(self.num_of_cols):
+                self.grid[y][x] = MAP_DICT["PAVED_ROAD"]
+        self.updateGrid()
+
+def main():
+    root = tk.Tk()
+    root.resizable(False, False)
+    root.eval('tk::PlaceWindow . center')
+    gui = Frame(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
